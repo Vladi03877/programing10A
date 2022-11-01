cda650ec58cf345d88bbb4bbd3981a4b89e68864
Vladi03877 <113976290+Vladi03877@users.noreply.github.com>
2022-11-01 21:40:51 +0200

    Add files via upload

---
+++ Domashna/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            n = n.ToLower();
+            bool a = n == "true";
+            if (a == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
