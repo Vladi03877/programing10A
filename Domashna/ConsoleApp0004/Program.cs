e61d85a075db720c6768c27b453854ebb2eeff6d
Vladi03877 <113976290+Vladi03877@users.noreply.github.com>
2022-11-11 23:37:28 +0200

    Add files via upload

---
+++ Domashna/ConsoleApp0004/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07.ExchangeVariableValues
+{
+    public class ExchangeVariableValues
+    {
+        static void Main()
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string c = Console.ReadLine();
+
+            string temp = c;
+            string temp2 = a;
+            a = temp;
+            c = temp2;
+
+
+            Console.WriteLine(a + b + c);
+
+        }
+    }
+}
